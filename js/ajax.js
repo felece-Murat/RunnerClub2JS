b6c8b52a6ce53c2b86ac9f2ebe755f9331a6cb92
Murat-Yilmaz <murat.yilmaz@felece.com>
2021-09-22 22:07:01 +0300

    Ajax istekleri.

--- js/ajax.js
+++ js/ajax.js
@@ -1,30 +1,44 @@
 var myRequest = new XMLHttpRequest;
 var secondReq = new XMLHttpRequest;
 myRequest.open('GET', `http://127.0.0.1:4000/container.json`);
-secondReq.open('GET', `http://127.0.0.1:4000/container.json`);
 
 // myRequest.onload = function() {
 //     console.log(myRequest.responseText);
 // };
 
-
-//JSON içerisindeki özel bir objeye ulaşmak.=
+// JSON içerisindeki özel bir objeye ulaşmak.
 myRequest.onload = function() {
-    //var myData = myRequest.responseText;
-    // console.log(myData[0]);  sadece parantezi alır.
-    var myData = JSON.parse(myRequest.responseText);
-    console.log(myData[0]); //çalışacak.
+  // var myData = myRequest.responseText;
+  // console.log(myData[0]);  sadece parantezi alır.
+  var myData = JSON.parse(myRequest.responseText);
+  console.log(myData[0]); //çalışacak.
 };
 myRequest.send();
 
 
-
 var btn = document.getElementById('ajaxSenderBtn');
 
+// Butona bağlamak.
 btn.addEventListener('click', function() { //event listener.
-    secondReq.onload = function() {
-        var myData = JSON.parse(secondReq.responseText);
-        console.log(myData[1]);
-    };
-    secondReq.send();
+  secondReq.open('GET', `http://127.0.0.1:3000/container.json`);
+  secondReq.onload = function() {
+    var myData = JSON.parse(secondReq.responseText);
+    renderHtml(myData)
+  };
+
+  //Error hadnling.
+  secondReq.onerror = function () {
+    console.log("error.");
+  }
+  secondReq.send();
+
+  //btn.style.display = 'none';
 });
+
+function renderHtml(data) {
+  var string = "";
+  for (var i = 0; i < data.length; i++) {
+    string += "<p>" + data[i].name + "</p>"
+  }
+  animalP.insertAdjacentHTML('beforeend' , string);
+}
